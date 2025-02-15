abf9b3757124ea5ec7e5fc738c2d9bde520c4e67
SonaliKhoseIntelalgos <sonali.khose@intelalgos.com>
2025-02-15 17:22:06 +0530

    done with navbar and sidebar

--- wtl-admin/app/page.js
+++ wtl-admin/app/page.js
@@ -9,10 +9,7 @@ export default function Home() {
         <Navbar />
         <main className="p-6 pt-20">
           {/* Table or content goes here */}
-          <div className="border bg-white p-4 rounded-md shadow-md">
-            <h2 className="text-gray-700 font-semibold">Customer Details</h2>
-            <p className="text-gray-500">Pickup | Drop | Trip Details | Trip Status</p>
-          </div>
+         
         </main>
       </div>
     </div>
